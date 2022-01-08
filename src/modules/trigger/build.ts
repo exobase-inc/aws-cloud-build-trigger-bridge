7d733c8c3a58e78ef29c75e09db799732d21c712
Ray Epps <rayharryepps@gmail.com>
2022-01-08 13:34:50 -0700

    add accidentally ignored module

---
+++ src/modules/trigger/build.ts
@@ -0,0 +1,51 @@
+import _ from 'radash'
+import { Props } from '@exobase/core'
+import { useLambda } from '@exobase/lambda'
+import { useApiKeyAuthentication } from '@exobase/auth'
+import { useJsonArgs } from '@exobase/hooks'
+import { CodeBuildClient, StartBuildCommand } from '@aws-sdk/client-codebuild'
+
+
+const config = {
+  projectName: process.env['AWS_CODE_BUILD_PROJECT_NAME'],
+  apiKey: process.env['BRIDGE_API_KEY']
+}
+
+type Args = {
+  command: string
+  args: any
+}
+
+async function triggerBuild({ args }: Props<Args>) {
+  const client = new CodeBuildClient({
+    region: 'us-east-1'
+  })
+  const command = new StartBuildCommand({
+    projectName: config.projectName,
+    environmentVariablesOverride: [{
+      name: 'TASK_ARGS',
+      value: Buffer.from(
+        JSON.stringify(args.args)
+      ).toString('base64')
+    }],
+    buildspecOverride: `
+version: 0.2
+phases:
+  build:
+    commands:
+      - ${args.command}
+
+`
+  })
+  await client.send(command)
+}
+
+export default _.compose(
+  useLambda(),
+  useApiKeyAuthentication(config.apiKey),
+  useJsonArgs<Args>(yup => ({
+    command: yup.string().required(),
+    args: yup.mixed().default({})
+  })),
+  triggerBuild
+)
